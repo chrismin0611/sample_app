46e31c4e05e73b586535b54e47d53adf7284c6f5
Kyong Uk Min <chrismin0611@gmail.com>
2015-08-26 01:24:53 -0500

    a

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,9 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
-		validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+		has_attached_file :images, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
 
 	else
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+		has_attached_file = images, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
 		
 
 		:storage => :dropbox,
