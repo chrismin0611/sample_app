47bd578cc56dd822c43d5a43439653e5f4d85cd1
Kyong Uk Min <chrismin0611@gmail.com>
2015-08-28 12:11:38 -0500

    sqlite error fixed

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,9 +7,10 @@ class Listing < ActiveRecord::Base
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
 		:storage => :dropbox,
 		:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-		:path => ":style/:id_:filename"
-		validates_attachment 
-		:image_content_type => { :content_type => ['image/jpg', 'image/jpeg', 'image/png']}, :message => 'file type is not allowed (only jpeg/png/gif images)'
+		:path => ":style/:id_:filename",
+		validates_attachment :image,
+		:image_content_type => { :content_type => ['image/jpg', 'image/jpeg', 'image/png']}, 
+		:message => 'file type is not allowed (only jpeg/png/gif images)'
 
 	end
 end
