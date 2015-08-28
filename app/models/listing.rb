fe22205b46623ef12e4aa62034d5a5332b11ff40
Kyong Uk Min <chrismin0611@gmail.com>
2015-08-28 13:25:15 -0500

    a

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,16 +1,13 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-		
+		do_not_validate_attachment_file_type :image
 
 	else
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
 		:storage => :dropbox,
 		:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 		:path => ":style/:id_:filename",
-		validates_attachment :image,
-		:image_content_type => { :content_type => ['image/jpg', 'image/jpeg', 'image/png']}, 
-		:message => 'file type is not allowed (only jpeg/png/gif images)'
-
+		do_not_validate_attachment_file_type :image
 	end
 end
